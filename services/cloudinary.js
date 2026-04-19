8eb671066ec0e34d1c54e8056701b3d6e730c3e2
code0305 <madayiakash@gmail.com>
2026-04-19 18:21:41 +0530

    Updated env

--- services/cloudinary.js
+++ services/cloudinary.js
@@ -4,7 +4,7 @@ import "dotenv/config"
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.API_KEY,
-    api_secret:process.env.SECRET_KEY
+    api_secret:process.env.CLOUD_SECRET_KEY,
 })
 
 export default cloudinary;
